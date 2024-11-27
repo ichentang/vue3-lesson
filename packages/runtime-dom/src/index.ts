d547695527b5d6aacdc2b747526d2351fdf1c0a7
tangshi <277338615@qq.com>
2024-11-27 22:09:23 +0800

    feat：实现虚拟DOM渲染

--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -1,8 +1,16 @@
 import { nodeOps } from './nodeOps';
 import patchProp from './patchProp';
 
+import { createRenderer } from '@vue/runtime-core';
+
 // 将节点操作和属性操作合在一起
 const renderOptions = Object.assign({ patchProp }, nodeOps);
 
-export { renderOptions };
-export * from '@vue/reactivity';
+// render方法采用domApi来进行渲染
+export const render = (vnode, container) => {
+  return createRenderer(renderOptions).render(vnode, container);
+};
+
+export * from '@vue/runtime-core';
+
+// runtime-dom -> runtime-core
